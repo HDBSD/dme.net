7353e6771fa46e58a43c7e9bf60f624bf922184c
Brad Sadd <bradley.sadd@gmail.com>
2022-01-16 21:53:08 +0000

    MemoryWatchTreeNode Implemented besides IO

--- DME.Net/MemoryWatcher/MemWatchTreeNode.cs
+++ DME.Net/MemoryWatcher/MemWatchTreeNode.cs
@@ -15,7 +15,7 @@ namespace DolphinComm.MemoryWatcher
         private string m_groupName;
         private MemWatchEntry m_entry;
         private List<MemWatchTreeNode> m_children;
-        private MemWatchEntry m_parent;
+        private MemWatchTreeNode m_parent;
 
         #endregion
 
@@ -29,17 +29,46 @@ namespace DolphinComm.MemoryWatcher
 
         public MemWatchTreeNode(ref MemWatchTreeNode node)
         {
-
+            m_isGroup = node.m_isGroup;
+            m_isValueEditing = node.m_isValueEditing;
+            m_groupName = node.m_groupName;
+            m_entry = node.m_entry;
+            m_children = node.m_children;
+            m_parent = node.m_parent;
         }
 
-        public MemWatchTreeNode()
+        ~MemWatchTreeNode()
         {
+            if (hasChildren())
+                m_children.Clear();
 
+            m_parent = null;
+            m_entry = null;
         }
 
         public bool isGroup()
         {
             return false;
+        }
+
+        public bool hasChildren()
+        {
+            return (m_children.Count >= 1);
+        }
+
+        public int childrenCount()
+        {
+            return m_children.Count;
+        }
+
+        public bool isValueEditing()
+        {
+            return m_isValueEditing;
+        }
+
+        public void setValueEditing(bool valueEditing)
+        {
+            m_isValueEditing = valueEditing;
         }
 
         public string getGroupName()
@@ -49,82 +78,79 @@ namespace DolphinComm.MemoryWatcher
 
         public void setGroupName(ref string groupName)
         {
-
+            m_groupName = groupName;
         }
 
         public MemWatchEntry getEntry()
         {
-            return (MemWatchEntry)m_entry;
+            return m_entry;
         }
 
         public void setEntry(MemWatchEntry entry)
         {
-
+            m_entry = null;
+            m_entry = entry;
         }
 
         public List<MemWatchTreeNode> getChildren()
         {
-            return this.m_children;
+            return m_children;
         }
 
         public void setChildren(List<MemWatchTreeNode> children)
         {
-
+            m_children = children;
         }
 
         public MemWatchTreeNode getParent()
         {
-            return this;
+            return m_parent;
         }
 
         public int getRow()
         {
+            if (m_parent == null)
+                return m_parent.m_children.IndexOf(this);
+
             return 0;
-        }
-
-        public bool isValueEditing()
-        {
-            return false;
-        }
-
-        public bool hasChildren()
-        {
-            return false;
-        }
-
-        public int childrenCount()
-        {
-            return 0;
-        }
-
-        public void setValueEditing(bool valueEditing)
-        {
-
         }
 
         public void appendChild(MemWatchTreeNode node)
         {
-
+            m_children.Append(node);
+            node.m_parent = this;
         }
 
         public void insertChild(int row, MemWatchTreeNode node)
         {
-
+            m_children.Insert(row, node);
+            node.m_parent = this;
         }
 
         public void removeChild(int row)
         {
-
+            m_children.RemoveAt(row);
         }
 
         public void clearAllChild()
         {
-
+            m_children.Clear();
         }
-        
-        //public void readFromJson(QJsonObject& json, MemWatchTreeNode parent = null);
-        //public void writeToJson(QJsonObject& json) const;
-        //public QString writeAsCSV() const;
+
+        public void readFromJson(string json, MemWatchTreeNode parent = null)
+        {
+            throw new NotImplementedException("json importing not implemented");
+        }
+        public void writeToJson(string json)
+        {
+            throw new NotImplementedException("json exporting not implemented");
+        }
+
+        public string writeAsCSV()
+        {
+            throw new NotImplementedException("CSV exporting not implemented");
+            return "";
+        }
 
         #endregion
     }
