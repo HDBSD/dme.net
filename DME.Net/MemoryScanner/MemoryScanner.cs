e6476673ef140728af9e39a41aa96a078a01fb90
Brad Sadd <bradley.sadd@gmail.com>
2022-01-18 02:29:36 +0000

    Added templates which are hopefull faithful to the original

--- DME.Net/MemoryScanner/MemoryScanner.cs
+++ DME.Net/MemoryScanner/MemoryScanner.cs
@@ -12,7 +12,7 @@ namespace DolphinComm.MemoryScanner
     public class MemScanner
     {
 
-        #region Enums
+    #region Enums
 
         public enum ScanFilter : uint
         {
@@ -39,7 +39,7 @@ namespace DolphinComm.MemoryScanner
 
         #endregion
 
-        #region Public
+    #region Public
         public MemScanner()
         {
 
@@ -69,14 +69,14 @@ namespace DolphinComm.MemoryScanner
 
         }
 
-        CompareResult compareMemoryAsNumber(byte[] first, byte[] second, byte[] offset, bool offsetInvert,
+        public CompareResult compareMemoryAsNumber(byte[] first, byte[] second, byte[] offset, bool offsetInvert,
                                             bool bswapSecond, int length)
         {
 
             return CompareResult.bigger;
         }
 
-        private T convertMemoryToType<T>(byte[] memory, bool invert)
+        public T convertMemoryToType<T>(byte[] memory, bool invert)
         {
             T theType = default(T);
             
@@ -89,13 +89,16 @@ namespace DolphinComm.MemoryScanner
 
             if (invert)
             {
-                throw new NotImplementedException("To Be completed");
+                dynamic tmp = theType;
+                tmp *= -1;
+                theType = (T)tmp;
             }
 
             return theType;
 
         }
 
+        /*
         private CompareResult compareMemoryAsNumbersWithType<T>(byte[] first, byte[] second, byte[] offset, bool offsetIntert, bool bswapSecond)
         {
             T firstByte;
@@ -126,15 +129,219 @@ namespace DolphinComm.MemoryScanner
             switch (size)
             {
                 case 2:
+                    UInt16 firstHalfword = 0;
+                    
+                    firstByte.
+
+
                 case 4:
-                case 6:
+                    UInt32 firstWord = 0;
                 case 8:
+                    UInt64 firstDoubleWord = 0;
+
                     
             }
 
 
         }
 
+    */
+        
+        public CompareResult compareMemoryAsNumbersWithType<T>(byte[] first, byte[] second, byte[] offset, bool offsetInvert, bool bswapSecond)
+        {
+            int size = Marshal.SizeOf(default(T));
+            
+            if (size == 2)
+            {
+                Common.SwapByteArray(ref first);
+
+                UInt16 firstByte = BitConverter.ToUInt16(first, 0);
+                UInt16 secondByte = 0;
+
+                if (bswapSecond)
+                {
+                    Common.SwapByteArray(ref second);
+                    secondByte = BitConverter.ToUInt16(second, 0);
+                }
+                else
+                {
+                    secondByte = BitConverter.ToUInt16(second, 0);
+                }
+
+                if (firstByte < (secondByte + convertMemoryToType<UInt16>(offset, offsetInvert)))
+                    return CompareResult.smaller;
+                else if (firstByte > (secondByte + convertMemoryToType<UInt16>(offset, offsetInvert)))
+                    return CompareResult.bigger;
+                else
+                    return CompareResult.equal;
+
+            }
+            else if (size == 4)
+            {
+                Common.SwapByteArray(ref first);
+
+                UInt32 firstByte = BitConverter.ToUInt32(first, 0);
+                UInt32 secondByte = 0;
+
+                if (bswapSecond)
+                {
+                    Common.SwapByteArray(ref second);
+                    secondByte = BitConverter.ToUInt32(second, 0);
+                }
+                else
+                {
+                    secondByte = BitConverter.ToUInt32(second, 0);
+                }
+
+                if (firstByte < (secondByte + convertMemoryToType<UInt32>(offset, offsetInvert)))
+                    return CompareResult.smaller;
+                else if (firstByte > (secondByte + convertMemoryToType<UInt32>(offset, offsetInvert)))
+                    return CompareResult.bigger;
+                else
+                    return CompareResult.equal;
+
+            }
+            else
+            {
+                Common.SwapByteArray(ref first);
+
+                UInt64 firstByte = BitConverter.ToUInt64(first, 0);
+                UInt64 secondByte = 0;
+
+                if (bswapSecond)
+                {
+                    Common.SwapByteArray(ref second);
+                    secondByte = BitConverter.ToUInt64(second, 0);
+                }
+                else
+                {
+                    secondByte = BitConverter.ToUInt64(second, 0);
+                }
+
+                if (firstByte < (secondByte + convertMemoryToType<UInt64>(offset, offsetInvert)))
+                    return CompareResult.smaller;
+                else if (firstByte > (secondByte + convertMemoryToType<UInt64>(offset, offsetInvert)))
+                    return CompareResult.bigger;
+                else
+                    return CompareResult.equal;
+            }
+
+        }
+
+        public void setType(Common.MemType type)
+        {
+
+        }
+
+        public void setBase(Common.MemBase memBase)
+        {
+
+        }
+
+        public void setEnforceMemAlignement(bool enforceAlignement)
+        {
+
+        }
+
+        public void setIsSigned(bool isSigned)
+        {
+
+        }
+
+
+        public List<UInt32> getResultsConsoleAddr()
+        {
+
+        }
+
+        public int getResultCount()
+        {
+
+        }
+
+        public int getTermsNumForFilter(Common.ScanFiter filter)
+        {
+
+        }
+
+        public Common.MemType getType()
+        {
+
+        }
+
+        public Common.MemBase getBase()
+        {
+
+        }
+
+        public int getLength()
+        {
+
+        }
+
+        public bool getIsUnsigned()
+        {
+
+        }
+
+        public string getFormattedScannedValueAt(int index)
+        {
+
+        }
+
+        public Common.MemOperationReturnCode updateCurrentRAMCache()
+        {
+
+        }
+
+        public string getFormattedCurrentValueAt(int index)
+        {
+
+        }
+
+        public void removeResultAt(int index)
+        {
+
+        }
+
+        public bool typeSupportsAdditionalOptions(Common.MemType type)
+        {
+
+        }
+
+        public bool hasScanStarted()
+        {
+
+        }
+
+
+        #endregion
+
+        #region Private
+
+        private bool isHitNextScan(Common.ScanFiter filter, byte[] memoryToCompare1,
+                            byte[] memoryToCompare2, byte[] noOffset,
+                            byte[] newerRAMCache, int realSize,
+                            UInt32 consoleOffset)
+        {
+            return false;
+        }
+
+        private string addSpacesToBytesArrays(ref string bytesArray)
+        {
+            return "";
+        }
+
+        private Common.MemType m_memType = Common.MemType.type_byte;
+        private Common.MemBase m_memBase = Common.MemBase.base_decimal;
+        private int m_memSize;
+        private bool m_enforceMemAlignement = true;
+        private bool m_memIsSigned = false;
+        private List<UInt32> m_resultsConsoleAddr;
+        private bool m_wasUnknownInitialValue = false;
+        private int m_resultCount = 0;
+        private byte[] m_scanRAMCache = null;
+        private bool m_scanStarted = false;
 
         #endregion
     }
